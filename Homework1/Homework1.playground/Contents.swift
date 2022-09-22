47a83b2024e11b52592fa1580e8349f89e271b97
Ali Kolukirik <aakolukirik@gmail.com>
2022-09-22 13:56:38 +0300

    Homework1

---
+++ Homework1/Homework1.playground/Contents.swift
@@ -0,0 +1,33 @@
+// Homework 1
+// Ali Ihsan Kolukirik
+
+import Foundation
+
+// Islem
+var islemNo : Int = 26
+var ogrNo : Int = 402
+var kitapNo : Int = 4236
+var aTarih : String = "22-09-2022"
+var vTarih : String = "29-09-2022"
+
+// Ogrenci
+var ogrAd : String = "Ali"
+var ogrSoyad : String = "Kolukırık"
+var cinsiyet : String = "Erkek"
+var dTarih : String = "22-03-1997"
+var sinif : String = "10-K"
+
+// Kitap
+var isbnNo : Int = 239482
+var kitapAdi : String = "Tutunamayanlar"
+var yazarNo : Int = 542348
+var turNo : Int = 5648
+var sayfaSayisi : Int = 652
+var puan : Double = 8.3
+
+// Yazar
+var yazarAd : String = "Oğuz"
+var yazarSoyad : String = "Atay"
+
+// Tur
+var turAdi : String = "Kurgu"
